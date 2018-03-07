697f29fff10b4893554eaca1343563e5889107a7
Massimiliano Pastore <35564114+masspastore@users.noreply.github.com>
2018-03-07 16:18:12 +0100

    Add files via upload

--- R/TCD.R
+++ R/TCD.R
@@ -8,9 +8,15 @@
 #fit <- lavaan::sem("PBB~PPP",data=Bullying)
 #yvar <- NULL
 
-TCD <- function(fit,yvar=NULL) {
-  if (is.null(yvar)) yvar <- setdiff(fit@Data@ov.names[[1]],fit@Data@ov.names.x[[1]])
+TCD <- function(fit,yvar=NULL,allmatrices=FALSE) {
   
+  # controllo se ci sono variabili latenti
+  (modelvar <- unique(unlist(fit@Model@dimNames[[1]])))
+  if (length(modelvar)!=length(fit@Data@ov.names[[1]])) {
+    stop("Attualmente non funziona con variabili latenti")
+  }
+  
+  if (is.null(yvar)) yvar <- setdiff(fit@Data@ov.names[[1]],fit@Data@ov.names.x[[1]])
   (PS <- lavaan::inspect(fit,"est")$psi[yvar,yvar])
   (Sy <- lavaan::fitted(fit)$cov[yvar,yvar])
   
@@ -19,7 +25,12 @@ TCD <- function(fit,yvar=NULL) {
   } else {
     TCD <- 1-det(PS)/det(Sy)  
   }
-  return(list(TCD=TCD,PS=PS,Sy=Sy))
+  
+  if (allmatrices) {
+    return(list(TCD=TCD,PS=PS,Sy=Sy))  
+  } else {
+    return(TCD)
+  }
 }
 
 #TCD(fit)
