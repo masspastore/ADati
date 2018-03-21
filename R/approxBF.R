0b80d6d796e66b94167d1b996875e8c80d1fb2d9
el <masspastore@gmail.com>
2018-03-21 11:39:18 +0100

    versione 5.0

--- R/approxBF.R
+++ R/approxBF.R
@@ -4,12 +4,15 @@ approxBF <- function(M0,M1) {
   (Bic0 <- BIC(M0))
   (Bic1 <- BIC(M1))
   (dBic <- Bic0-Bic1)
-  (BF <- exp(-dBic/2))
+  (BF <- exp(dBic/2))
   
-  (support <- ifelse(dBic<=0,"supports H0","supports H1"))
-  
+  (support <- ifelse(dBic<=0,"supports M0","supports M1"))
+
   cat("-------------------","\n")
-  cat(paste("delta.BIC",support),"\n")
+  cat(paste("Bayes Factor",support),"\n")
+  if (dBic<=0) print(formula(M0))
+  if (dBic>0) print(formula(M1))
+  
   cat("-------------------","\n")
   cat(" ","\n")
   return(list(dBIC=dBic,BF=BF))
---
+++ R/lmer_check.R
@@ -0,0 +1,38 @@
+## diagnostic plot for lmer objects
+lmer_check <- function(fit,all=TRUE) {
+  
+  par(ask=!all)
+  
+  ## aggrego dati per i grafici
+  dati <- data.frame(fitted=fitted(fit),resid=resid(fit))
+  dati$absres <- sqrt(abs(dati$resid))
+  dati$lev <- hatvalues(fit)
+  
+  theme_set(theme_bw())
+  P1 <- ggplot(dati,aes(fitted,resid))+geom_hline(yintercept = 0,lty="longdash")+geom_point()+
+    geom_smooth()+xlab("Fitted values")+ylab("Residuals")+ggtitle("Residuals vs Fitted")+
+    theme(plot.title =element_text(hjust=.5))
+  
+  P2 <- ggplot(dati,aes(sample=resid))+geom_abline(aes(slope=sd(resid),intercept=mean(resid)),lty=2)+
+    stat_qq()+ggtitle("Normal Q-Q")+xlab("Theoretical Quantiles")+ylab("Standardized Residuals")+
+    theme(plot.title =element_text(hjust=.5))
+  
+  P3 <- ggplot(dati,aes(fitted,absres))+geom_point()+
+    geom_smooth()+xlab("Fitted values")+ylab(expression(sqrt("|Standardized residuals|")))+
+    ggtitle("Scale-Location")+theme(plot.title =element_text(hjust=.5))
+  
+  MAXLEV <- max(dati$lev)
+  P4 <- ggplot(dati,aes(lev,resid))+geom_point()+
+    scale_x_continuous(limits=c(0,MAXLEV)) +  
+    geom_smooth()+xlab("Leverage")+ylab("Standardized residuals")+ggtitle("Residuals vs Leverage")+
+    theme(plot.title =element_text(hjust=.5))
+  
+  PLOT <- list(P1,P2,P3,P4)
+  
+  if (all) {
+    cowplot::plot_grid(plotlist = PLOT)  
+  } else {
+    for (j in 1:length(PLOT)) print(PLOT[[j]])
+  }
+  
+}
