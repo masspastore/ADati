5a6520cc74258613c758daac93188da7415d12d3
Massimiliano Pastore <35564114+masspastore@users.noreply.github.com>
2024-04-10 20:59:16 +0200

    Add files via upload

--- R/bayes_eta2.R
+++ R/bayes_eta2.R
@@ -3,6 +3,10 @@
 #partial <- FALSE
 #posterior <- FALSE
 
+#X <- foreign::read.spss( "~/MEGAsync/didattica/ADsvil2324/lezioni/data/auto.sav",
+#                         to.data.frame = TRUE)
+#fit <- rstanarm::stan_glm( consumo ~ peso, data = X, refresh = 0 )
+#
 #fit <- rstanarm::stan_glm( ChildBeh ~ ParSty * HighSens, 
 #                           data = ESdata, chains = 1 ) 
 #fit <- rstanarm::stan_glm( ChildBeh ~ 1, 
@@ -22,70 +26,72 @@
 bayes_eta2 <- function(fit, partial = FALSE, 
                        posterior = FALSE) {
   
-  package <- ifelse( class(fit)=="brmsfit", "brms", "rstanarm")
+  package <- ifelse(class(fit) == "brmsfit", "brms", "rstanarm")
   if (package[1] == "rstanarm") {
-    coef.names <- names(coef(fit))
-    npred <- length( fit$coefficients )
-    FORMULA <- paste(as.character(formula(fit))[c(2,1,3)],collapse = " ")
-  } else {
+    coef.names <- names(rstanarm::fixef(fit))
+    npred <- length(rstanarm::fixef(fit))
+    FORMULA <- paste(as.character(formula(fit))[c(2, 1, 
+                                                  3)], collapse = " ")
+  }
+  else {
     coef.names <- rownames(brms::fixef(fit))
     npred <- nrow(brms::fixef(fit))
     FORMULA <- as.character(formula(fit))[1]
   }
-
-  PREDS <- strsplit(FORMULA,split = "~")[[1]][2]
-  FORMULA <- paste0("y ~",PREDS)
+  PREDS <- strsplit(FORMULA, split = "~")[[1]][2]
+  FORMULA <- paste0("y ~", PREDS)
   
   Z <- fit$data
+  NA.count <- apply(Z,2,function(x){sum(is.na(x))})
   
-  if (package[1] == "rstanarm") {
-    pp <- data.frame( t( rstanarm::posterior_predict( fit ) ) )
-  } else {
-    pp <- data.frame( t( brms::posterior_predict( fit ) ) )
+  if (sum(NA.count>0)>0) {
+    stop("Non gestisce (ancora) modelli con dati mancanti.")
   }
   
+  
+  if (package[1] == "rstanarm") {
+    pp <- data.frame(t(rstanarm::posterior_predict(fit)))
+  }
+  else {
+    pp <- data.frame(t(brms::posterior_predict(fit)))
+  }
   if (npred == 1) {
     warning("null model, eta-squared is 0")
   }
-  
   if (npred == 2) {
-    
-    ETApost <- data.frame( matrix( sapply(1:ncol(pp), function(j) {
-      Z$y <- pp[,j]
-      fit <- lm( FORMULA, data = Z )
-      ETA <- heplots::etasq( fit, partial = partial )[,1]
+    ETApost <- data.frame(matrix(sapply(1:ncol(pp), function(j) {
+      Z$y <- pp[, j]
+      fit <- lm(FORMULA, data = Z)
+      ETA <- heplots::etasq(fit, partial = partial)[, 
+                                                    1]
       ETA <- ETA[-length(ETA)]
       return(ETA)
-    }),ncol = 1))
-    
+    }), ncol = 1))
     colnames(ETApost) <- coef.names[-1]
-  } 
-  
-  if (npred >2) {
-    ETApost <- data.frame( t( sapply(1:ncol(pp), function(j){
-      
-      Z$y <- pp[,j]
-      fit <- lm( FORMULA, data = Z )
-      ETA <- heplots::etasq( fit, partial = partial )[,1]
+  }
+  if (npred > 2) {
+    ETApost <- data.frame(t(sapply(1:ncol(pp), function(j) {
+      Z$y <- pp[, j]
+      fit <- lm(FORMULA, data = Z)
+      ETA <- heplots::etasq(fit, partial = partial)[, 
+                                                    1]
       ETA <- ETA[-length(ETA)]
       names(ETA) <- coef.names[-1]
       return(ETA)
-      
     })))
   }
-  
-  if (npred >1) {
-    ETAstat <- data.frame(
-      Estimate = apply(ETApost,2,median),
-      Error = apply(ETApost,2,mad)
-    )
-    
+  if (npred > 1) {
+    ETAstat <- data.frame(Estimate = apply(ETApost, 2, median), 
+                          Error = apply(ETApost, 2, mad))
     if (posterior) {
-      return(list(ETAstat=ETAstat, ETApost = ETApost ))
-    } else {
+      return(list(ETAstat = ETAstat, ETApost = ETApost))
+    }
+    else {
       return(ETAstat)
     }
-  } 
+  }
   
 }
+
+#bayes_eta2( fit )
 
