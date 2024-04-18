76570063f428c0b08139f577df8204f21766011b
Massimiliano Pastore <35564114+masspastore@users.noreply.github.com>
2024-04-18 15:49:12 +0200

    Add files via upload

--- R/plot_relative_evidence.R
+++ R/plot_relative_evidence.R
@@ -46,11 +46,11 @@ plot_relative_evidence <- function(weights,labels=NULL,log=TRUE,
       }
     }
   }
-  rownames(RR) <- colnames(RR) <- names(weights)
+  rownames(RR) <- colnames(RR) <- names(w)
   RRplot <- reshape::melt(RR)
   colnames(RRplot)[1:2] <- c("X1","X2")
-  RRplot$X2 <- factor(RRplot$X2, levels = names(weights)[length(weights):1], ordered = TRUE)
-  RRplot$X1 <- factor(RRplot$X1, levels = names(weights)[length(weights):1], ordered = TRUE)
+  RRplot$X2 <- factor(RRplot$X2, levels = names(w)[length(w):1], ordered = TRUE)
+  RRplot$X1 <- factor(RRplot$X1, levels = names(w)[length(w):1], ordered = TRUE)
   
   if (short.names) {
     levels(RRplot$X2) <- paste0("(",1:length(levels(RRplot$X2)),")")
