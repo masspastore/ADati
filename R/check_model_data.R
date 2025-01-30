d8ed384cd6318c066a1ffc0db10a938cfaa434ae
Massimiliano Pastore <35564114+masspastore@users.noreply.github.com>
2025-01-30 09:47:36 +0100

    Add files via upload

--- R/check_model_data.R
+++ R/check_model_data.R
@@ -1,7 +1,16 @@
 
 check_model_data <- function(fit, cook_levels = c(.5,1)) {
 
-  modelData <- fit$data
+  # per gestire i mancanti
+  coef.names <- names(rstanarm::fixef(fit))
+  npred <- length(rstanarm::fixef(fit))
+  FORMULA <- formula(fit)
+  X <- attr(terms(FORMULA), which = "term.labels")
+  Y <- as.character(attr(terms(FORMULA), which = "variables")[[2]])
+  FORMULA <- paste0("y ~ ",paste(X, collapse=" + "))
+  PREDS <-  X <- X[!grepl(":",X)] 
+  
+  modelData <- na.omit( fit$data[c(Y,PREDS)] )
   modelData$.x <- 1:nrow(modelData)
   modelData$.resid <- residuals(fit)  
   modelData$.fitted <- fitted(fit)
